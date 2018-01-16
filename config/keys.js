afbad15cfff54d2718e0091cf93a5fa7ab0a0496
Travis Young <t.w.young.ubc@gmail.com>
2018-01-16 09:26:23 -0800

    added cookie session

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,6 @@ module.exports = {
   googleClientID:
     '454188717509-1ti6hihthec7aajfr2g2jriku9u75ijq.apps.googleusercontent.com',
   googleClientSecret: 'YqcVP1vcNT8P1EI1rAzBgwL5',
-  mongoURI: "mongodb://travis:password@ds257627.mlab.com:57627/radtrav-emaily-dev"
+  mongoURI: "mongodb://travis:password@ds257627.mlab.com:57627/radtrav-emaily-dev",
+  cookieKey: 'adfafdadifkjhjkkkljhlkhlakdhflkasdhflahsdflasdfadfasdflkjhasldjhf'
 };
