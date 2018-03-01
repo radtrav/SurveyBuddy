3adf3bbe64051e9a45ca28528e7a684950688ebb
Travis Young <t.w.young.ubc@gmail.com>
2018-02-28 16:27:15 -0800

    error handling for mailer.send()

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,16 @@
-module.exports = (survey) => {
-  return '<div>' + survey.body +  '</div>';
+module.exports = survey => {
+  return `
+    <html>
+       <body>
+            <div style="text-align: center;"> 
+                <h3> I'd like your input</h3>
+                <p> Please answer the following question</p>
+                <p> ${survey.body}</p>
+                <div> <a href="http://localhost:3000"> YES </a></div>
+                <div> <a href="http://localhost:3000"> NO </a></div>
+            </div> 
+        </body>
+    </html>
+  
+  `;
 };
