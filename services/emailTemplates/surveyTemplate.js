49d684391a236ee9967428ecb351f492039471a6
Travis Young <t.w.young.ubc@gmail.com>
2018-02-28 17:23:35 -0800

    redirect on feedback

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,5 @@
+const keys = require('../../config/keys');
+
 module.exports = survey => {
   return `
     <html>
@@ -6,8 +8,8 @@ module.exports = survey => {
                 <h3> I'd like your input</h3>
                 <p> Please answer the following question</p>
                 <p> ${survey.body}</p>
-                <div> <a href="http://localhost:3000"> YES </a></div>
-                <div> <a href="http://localhost:3000"> NO </a></div>
+                <div> <a href="${keys.redirectDomain}/api/surveys/thanks"> YES </a></div>
+                <div> <a href="${keys.redirectDomain}/api/surveys/thanks"> NO </a></div>
             </div> 
         </body>
     </html>
