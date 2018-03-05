8d96261ede586d73988fd0f6a2a4362838d80e03
Travis Young <t.w.young.ubc@gmail.com>
2018-03-05 15:57:02 -0800

    View data with pie charts

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -1,5 +1,6 @@
 import React, { Component} from 'react';
 import PropTypes from 'prop-types';
+import PieChart from 'react-minimal-pie-chart';
 import { connect } from 'react-redux';
 
 import { fetchSurveys } from '../../actions';
@@ -11,17 +12,29 @@ class SurveyList extends Component {
   renderSurveys() {
     return this.props.surveys.reverse().map(survey => {
       return (
-        <div key={survey._id} className="card darken- 1">
-          <div className="card-content">
-            <span className="card-title"> {survey.title}</span>
-            <p>{survey.body}</p>
-            <p className="right">
-              Sent on: {new Date(survey.dateSent).toLocaleDateString()}
-            </p>
+        <div style={{padding: 40}} key={survey._id} className="card darken- 1">
+          <div className="row">
+            <div className="col s4">
+              <PieChart
+                data={[
+                  { value: parseInt(survey.yes), key: 1, color: '#E38627' },
+                  { value: parseInt(survey.no), key: 2, color: '#C13C37' },
+                ]}
+              />
+            </div>
+            <div className="col s8 card-content">
+              <span className="card-title"> {survey.title}</span>
+              <p>{survey.body}</p>
+              <p className="right">
+                Sent on: {new Date(survey.dateSent).toLocaleDateString()}
+              </p>
+            </div>
+
           </div>
+
           <div className="card-action">
-            <a href="#"> YES: {survey.yes}</a>
-            <a href="#"> NO: {survey.no}</a>
+            <a style={{color: '#E38627' }} href="#"> YES: {survey.yes}</a>
+            <a style={{color: '#C13C37' }} href="#"> NO: {survey.no}</a>
           </div>
         </div>
       );
