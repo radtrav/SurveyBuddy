70e1762f4b97115f939ac3f7c1f798f4698ffc3e
Travis Young <t.w.young.ubc@gmail.com>
2018-03-05 11:51:51 -0800

    Added process.env variable for publishable key

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -15,7 +15,8 @@ class Payments extends Component {
         description="5$ for 5 email credits"
         amount={500}
         token={token => this.props.handleToken(token)}
-        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+        // stripeKey={process.env.REACT_APP_STRIPE_KEY}
+        stripeKey={process.env.STRIPE_PUBLISHABLE_KEY}
       >
         <button className="btn"> Add Credits </button>
       </StripeCheckout>
